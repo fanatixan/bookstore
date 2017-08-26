78d97b4bd7338ea709e0f1d7f913b4fbcc6e899d
fanatixan <attila@javadev.hu>
2017-08-26 14:42:53 +0200

    2.3

--- src/main/java/hu/javadev/bookstore/Main.java
+++ src/main/java/hu/javadev/bookstore/Main.java
@@ -3,6 +3,7 @@ package hu.javadev.bookstore;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import hu.javadev.bookstore.model.Author;
+import hu.javadev.bookstore.model.Book;
 import hu.javadev.bookstore.model.Publisher;
 
 public class Main {
@@ -18,6 +19,12 @@ public class Main {
 
         Publisher publisher = context.getBean("publisher", Publisher.class);
         System.out.println(publisher);
+
+        Book _1984 = context.getBean("1984", Book.class);
+        System.out.println(_1984);
+
+        Book apk = context.getBean("apk", Book.class);
+        System.out.println(apk);
 
         context.close();
     }
